e64286eacbd164dc99a417171191bb07c90037fa
cristalNova <cliffordt129@gmail.com>
2025-10-26 19:26:07 -0500

    feat: communication between login page and chat page fixed to pass username

--- web-client/src/pages/Log_in.js
+++ web-client/src/pages/Log_in.js
@@ -1,6 +1,7 @@
 import Button from "../components/ActionButton.js";
 import TextInput from "../components/InputMessageBar.js";
 import Title from "../components/Title.js";
+import { navigate } from "../router/Router.js";
 
 const Login = () => {
     const loginPage = document.createElement("div");
@@ -9,18 +10,28 @@ const Login = () => {
     const form = document.createElement("form");
     form.classList.add("login-form");
 
-    const title = Title("Welcome to Chat App");
+    const title = Title("Bienvenido a Chat App");
     form.appendChild(title);
 
     const input = TextInput("Username","");
     form.appendChild(input);
     const btnLogin = Button({
-        text: "Log In",
+        text: "Ingresar",
         icon: "",
         type: "submit"
     });
+
+    form.onsubmit = (event) => {
+        event.preventDefault();
+        const username = input.value.trim();
+        if (!username) return;
+
+        navigate("/chat", { username });
+
+    }
+
     form.appendChild(btnLogin);
-    
+
     loginPage.appendChild(form);
 
     return loginPage;
--- web-client/src/router/Router.js
+++ web-client/src/router/Router.js
@@ -2,12 +2,19 @@ export const Router = (paths) => {
 
     const route = window.location.pathname;
 
+    const state = window.history.state || {};
+
     const routeComponent = paths[route] || (() => {
         const notFound = document.createElement("p");
         notFound.innerText = "404 - Not Found";
         return notFound;
     });
 
-    return routeComponent();
+    return routeComponent(state);
 
+};
+
+export const navigate = (path, params = {}) => {
+    window.history.pushState(params, "", path);
+    window.renderApp();
 };
